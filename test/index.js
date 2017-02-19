054ae7b80dda4ee3b3d8f56bbba6211f634c003a
Beautisos <Beautisos@beautisos.local>
2017-02-19 13:39:47 -0500

    docs, tests, code hints

--- test/index.js
+++ test/index.js
@@ -15,13 +15,18 @@ describe('reload',function(){
       assert.equal(typeof setup.port, 'number')
       
       config = setup
-      
+    })
+
+    //request missing index2.html
+    .then(function(){
       return promiseRequest({host:'localhost', port:'3030', path:'/index2.html'})
     })
     .then(function(res){
       assert.equal(res.statusCode, 404)
       assert.equal(res.statusMessage, 'Not Found')
     })
+
+    //request index and then test response body
     .then(function(){
       return promiseRequest({host:'localhost', port:'3030', path:'/index.html'})
     })
@@ -39,6 +44,26 @@ describe('reload',function(){
     .then(function(body){
       assert.equal(body.search(/reload\/reload\.js/)>=0, true)
     })
+
+    //request client-reload script and then test response body
+    .then(function(){
+      return promiseRequest({host:'localhost', port:'3030', path:'/reload/reload.js'})
+    })
+    .then(function(response){
+      assert.equal(response.statusCode, 200)
+      assert.equal(response.statusMessage, 'OK')
+      
+      response.setEncoding('utf8');
+      return new Promise(function(res,rej){
+        response.on('data', function (chunk) {
+          res(chunk)
+        })
+      })
+    })
+    .then(function(body){
+      assert.equal(body.search(/Reload/)>=0, true)
+    })
+
     .then(function(){
       config.httpServer.close(done)
     })
