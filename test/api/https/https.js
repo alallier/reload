fced913494b369908d779eec01f200003a194a0b
Alexander J. Lallier <alexanderlallier@aol.com>
2018-12-28 14:45:41 -0500

    * Added ability to pass files instead of filenames for HTTPS certificates * Added support for P12/PFX HTTPS files * Added forceWss option which forces the client to connect over `wss` even when the window is `https`

--- test/api/https/https.js
+++ test/api/https/https.js
@@ -14,72 +14,185 @@ const testKeyLocation = path.join(__dirname, '/test-only.key')
 const testCrtLocation = path.join(__dirname, '/test-only.crt')
 const testCaPemLocation = path.join(__dirname, '/test-only-ca.pem')
 
-const testKey = fs.readFileSync(testKeyLocation, 'utf-8')
-const testCrt = fs.readFileSync(testCrtLocation, 'utf-8')
-const testCaPem = fs.readFileSync(testCaPemLocation, 'utf-8')
+const testP12Location = path.join(__dirname, '/test-only.pfx')
+
+const testKey = fs.readFileSync(testKeyLocation)
+const testCrt = fs.readFileSync(testCrtLocation)
+const testCaPem = fs.readFileSync(testCaPemLocation)
+
+const testP12 = fs.readFileSync(testP12Location)
+
+console.log(typeof testKey)
 
 describe('HTTPS', function () {
-  it('Should create (default) `/reload/reload.js` route for reload file', async () => {
-    var app = express()
-
-    try {
-      var reloadReturned = await reload(app, { https: { key: testKeyLocation, cert: testCrtLocation } })
-    } catch (err) {
-      console.error('IN ERROR', err)
-    }
-
-    var response = await helperFunction.makeRequest('/reload/reload.js', app, { https: { key: testKey, cert: testCrt, ca: testCaPem } })
-
-    await helperFunction.closeReloadSocket(reloadReturned)
-
-    assert.equal(response, 200)
-  })
-
-  it('Should error with bad key file', async () => {
+  it('Should error if `certAndKey` and `p12` are not defined in HTTPS options', async () => {
     sinon.stub(console, 'log').returns(void 0)
     sinon.stub(console, 'error').returns(void 0)
 
     var app = express()
 
     try {
-      await reload(app, { https: { key: path.join('/somethingElse', testKeyLocation), cert: testCrtLocation } })
+      await reload(app, { https: { } })
     } catch (err) {
       console.log.restore()
       console.error.restore()
 
-      assert.equal(err.message, 'Could not initialize reload could not read key file')
+      assert.equal(err.message, 'Could not initialize reload HTTPS setup incorrectly. Make sure to define a `p12` or `certAndKey` in the HTTPS options')
     }
   })
 
-  it('Should error with bad cert file', async () => {
-    sinon.stub(console, 'log').returns(void 0)
-    sinon.stub(console, 'error').returns(void 0)
+  describe('HTTPS - Cert and Key', function () {
+    it('Should create (default) `/reload/reload.js` route for reload file', async () => {
+      var app = express()
 
-    var app = express()
+      try {
+        var reloadReturned = await reload(app, { https: { certAndKey: { key: testKeyLocation, cert: testCrtLocation } } })
+      } catch (err) {
+        console.error('IN ERROR', err)
+      }
 
-    try {
-      await reload(app, { https: { key: testKeyLocation, cert: path.join('/somethingElse', testCrtLocation) } })
-    } catch (err) {
-      console.log.restore()
-      console.error.restore()
+      var response = await helperFunction.makeRequest('/reload/reload.js', app, { https: { key: testKey, cert: testCrt, ca: testCaPem } })
 
-      assert.equal(err.message, 'Could not initialize reload could not read cert file')
-    }
+      await helperFunction.closeReloadSocket(reloadReturned)
+
+      assert.equal(response, 200)
+    })
+
+    it('Should accept HTTPS key as file contents', async () => {
+      var app = express()
+
+      try {
+        var reloadReturned = await reload(app, { https: { certAndKey: { key: testKey, cert: testCrtLocation } } })
+      } catch (err) {
+        console.error('IN ERROR', err)
+      }
+
+      var response = await helperFunction.makeRequest('/reload/reload.js', app, { https: { key: testKey, cert: testCrt, ca: testCaPem } })
+
+      await helperFunction.closeReloadSocket(reloadReturned)
+
+      assert.equal(response, 200)
+    })
+
+    it('Should accept HTTPS cert as file contents', async () => {
+      var app = express()
+
+      try {
+        var reloadReturned = await reload(app, { https: { certAndKey: { key: testKey, cert: testCrt } } })
+      } catch (err) {
+        console.error('IN ERROR', err)
+      }
+
+      var response = await helperFunction.makeRequest('/reload/reload.js', app, { https: { key: testKey, cert: testCrt, ca: testCaPem } })
+
+      await helperFunction.closeReloadSocket(reloadReturned)
+
+      assert.equal(response, 200)
+    })
+
+    it('Should error with bad key file', async () => {
+      var app = express()
+
+      try {
+        var reloadReturned = await reload(app, { https: { certAndKey: { key: path.join('/somethingElse', testKeyLocation), cert: testCrtLocation } } })
+      } catch (err) {
+        return assert(true)
+      }
+
+      await helperFunction.closeReloadSocket(reloadReturned)
+      return assert(false)
+    })
+
+    it('Should error with bad cert file', async () => {
+      var app = express()
+
+      try {
+        var reloadReturned = await reload(app, { https: { certAndKey: { key: testKeyLocation, cert: path.join('/somethingElse', testCrtLocation) } } })
+      } catch (err) {
+        return assert(true)
+      }
+
+      await helperFunction.closeReloadSocket(reloadReturned)
+      return assert(false)
+    })
+
+    it('Should create secure WebSocket on default port', async () => {
+      var app = express()
+
+      try {
+        var reloadReturned = await reload(app, { https: { certAndKey: { key: testKeyLocation, cert: testCrtLocation } } })
+      } catch (err) {
+        assert(false, err)
+      }
+
+      var result = await helperFunction.testWebSocket(9856, true, { key: testKey, cert: testCrt, ca: testCaPem })
+
+      await helperFunction.closeReloadSocket(reloadReturned)
+
+      assert.equal(result, true)
+    })
   })
 
-  it('Should create secure WebSocket on default port', async () => {
-    var app = express()
+  describe('HTTPS - PFX', function () {
+    it('Should create (default) `/reload/reload.js` route for reload file', async () => {
+      var app = express()
 
-    try {
-      var reloadReturned = await reload(app, { https: { key: testKeyLocation, cert: testCrtLocation } })
-    } catch (err) {
+      try {
+        var reloadReturned = await reload(app, { https: { p12: { p12Path: testP12Location, passphrase: '1234' } } })
+      } catch (err) {
+        assert(false, err)
+      }
 
-    }
+      var response = await helperFunction.makeRequest('/reload/reload.js', app, { https: { key: testKey, cert: testCrt, ca: testCaPem } })
 
-    var result = await helperFunction.testWebSocket(9856, true, { key: testKey, cert: testCrt, ca: testCaPem })
+      await helperFunction.closeReloadSocket(reloadReturned)
 
-    await helperFunction.closeReloadSocket(reloadReturned)
+      assert.equal(response, 200)
+    })
 
-    assert.equal(result, true)
+    it('Should accept pfx as file contents', async () => {
+      var app = express()
+
+      try {
+        var reloadReturned = await reload(app, { https: { p12: { p12Path: testP12, passphrase: '1234' } } })
+      } catch (err) {
+        assert(false, err)
+      }
+
+      var response = await helperFunction.makeRequest('/reload/reload.js', app, { https: { key: testKey, cert: testCrt, ca: testCaPem } })
+
+      await helperFunction.closeReloadSocket(reloadReturned)
+
+      assert.equal(response, 200)
+    })
+
+    it('Should error with bad pxf file', async () => {
+      var app = express()
+
+      try {
+        var reloadReturned = await reload(app, { https: { p12: { p12Path: path.join('/somethingElse', testP12Location), passphrase: '1234' } } })
+      } catch (err) {
+        return assert(true)
+      }
+
+      await helperFunction.closeReloadSocket(reloadReturned)
+      return assert(false)
+    })
+
+    it('Should create secure WebSocket on default port', async () => {
+      var app = express()
+
+      try {
+        var reloadReturned = await reload(app, { https: { p12: { p12Path: testP12Location, passphrase: '1234' } } })
+      } catch (err) {
+        assert(false, err)
+      }
+
+      var result = await helperFunction.testWebSocket(9856, true, { key: testKey, cert: testCrt, ca: testCaPem })
+
+      await helperFunction.closeReloadSocket(reloadReturned)
+
+      assert.equal(result, true)
+    })
   })
 })
